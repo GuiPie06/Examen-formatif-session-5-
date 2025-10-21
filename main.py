08e9ed42f4d1ce650a2aadfa0d68f2d0851771b3
6245102 <6245102@ctsjean.qc.ca>
2025-10-21 16:25:38 -0400

    0

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import json, csv
 import tkinter as tk
-from tkinter import ttk
+from logging import exception
+from tkinter import ttk, messagebox, filedialog
+import re
 
+FIELDS = ("produit", "quantite", "prix")
 
 class Produit:
     def __init__(self, name, quantity, price):
@@ -39,6 +43,7 @@ class App(tk.Tk):
 
 
         self.creer_widgets()
+        self.rows = []
 
 
 
@@ -47,69 +52,78 @@ class App(tk.Tk):
         self.rowconfigure(0, weight=1)
 
         content = tk.Frame(self)
-        content.grid(row=0, column=0, sticky="new")
+        content.grid(row=0, column=0, sticky="new", padx= 10)
         content.rowconfigure(0, weight=1)
-        content.columnconfigure(0, weight=1)
         content.rowconfigure(1, weight=1)
-        content.columnconfigure(1, weight=1)
+        content.rowconfigure(2, weight=1)
 
 
 
         self.labelframe_ajouter = tk.LabelFrame(content, text="Ajouter un produit")
-        self.labelframe_ajouter.grid(row=0, column=0, sticky="ew")
+        self.labelframe_ajouter.grid(row=0, column=0, sticky="ew",padx= 10, pady= 10)
 
-        self.labelframe_ajouter.rowconfigure(0, weight=1)
-        self.labelframe_ajouter.rowconfigure(1, weight=1)
-        self.labelframe_ajouter.rowconfigure(2, weight=1)
-        self.labelframe_ajouter.columnconfigure(0, weight=1)
+        self.labelframe_ajouter.rowconfigure(0, weight=0)
+        self.labelframe_ajouter.rowconfigure(1, weight=0)
+        self.labelframe_ajouter.rowconfigure(2, weight=0)
+        self.labelframe_ajouter.columnconfigure(0, weight=0)
         self.labelframe_ajouter.columnconfigure(1, weight=2)
-        self.labelframe_ajouter.columnconfigure(2, weight=1)
+        self.labelframe_ajouter.columnconfigure(2, weight=0)
 
         self.label_produit = tk.Label(self.labelframe_ajouter, text="Produit:")
-        self.label_produit.grid(row=0, column=0, sticky="ew")
+        self.label_produit.grid(row=0, column=0, sticky="ew", padx=10, pady=5)
         self.entry_produit = tk.Entry(self.labelframe_ajouter)
         self.entry_produit.grid(row=0, column=1, sticky="ew")
 
         self.label_quantite = tk.Label(self.labelframe_ajouter, text="Quantité:")
-        self.label_quantite.grid(row=1, column=0, sticky="ew")
+        self.label_quantite.grid(row=1, column=0, sticky="ew", padx=10, pady=5)
         self.entry_quantite = tk.Entry(self.labelframe_ajouter)
         self.entry_quantite.grid(row=1, column=1, sticky="ew")
 
         self.label_prix = tk.Label(self.labelframe_ajouter, text="Quantité:")
-        self.label_prix.grid(row=2, column=0, sticky="ew")
+        self.label_prix.grid(row=2, column=0, sticky="ew", padx=10, pady=5)
         self.entry_prix = tk.Entry(self.labelframe_ajouter)
         self.entry_prix.grid(row=2, column=1, sticky="ew")
 
-        self.bouton_ajouter = tk.Button(self.labelframe_ajouter, text="Ajouter un Produit")
-        self.bouton_ajouter.grid(row=1, column=2, sticky="ew")
+        self.bouton_ajouter = tk.Button(self.labelframe_ajouter, text="Ajouter un Produit", command=self.ajouter)
+        self.bouton_ajouter.grid(row=1, column=2, sticky="ew", padx=10)
 
 
 
 
         self.labelframe_gestion = tk.LabelFrame(content, text="Gestion des produits")
-        self.labelframe_gestion.grid(row=1, column=0, sticky="ew")
+        self.labelframe_gestion.grid(row=1, column=0, sticky="ew",padx= 10, pady= 10)
 
-        self.bouton_supprimer = tk.Button(self.labelframe_gestion, text="Supprimer Produit")
-        self.bouton_supprimer.grid(row=0, column=0, sticky="", columnspan=2)
+        self.labelframe_gestion.rowconfigure(0, weight=1)
+        self.labelframe_gestion.rowconfigure(1, weight=1)
+        self.labelframe_gestion.columnconfigure(0, weight=1)
+        self.labelframe_gestion.columnconfigure(1, weight=1)
+        self.labelframe_gestion.columnconfigure(2, weight=1)
+        self.labelframe_gestion.columnconfigure(3, weight=1)
+
+        self.bouton_supprimer = tk.Button(self.labelframe_gestion, text="Supprimer Produit", command=self.supprimer)
+        self.bouton_supprimer.grid(row=0, column=0, sticky="", columnspan=2, pady=10)
 
         self.bouton_modifier = tk.Button(self.labelframe_gestion, text="Modifier Produit")
-        self.bouton_modifier.grid(row=0, column=2, sticky="", columnspan=2)
+        self.bouton_modifier.grid(row=0, column=2, sticky="", columnspan=2, pady=10)
 
         self.bouton_sauvgarder_csv = tk.Button(self.labelframe_gestion, text="Sauvgarder CSV")
-        self.bouton_sauvgarder_csv.grid(row=1, column=0, sticky="")
+        self.bouton_sauvgarder_csv.grid(row=1, column=0, sticky="", pady=(10,20))
 
-        self.bouton_sauvgarder_json = tk.Button(self.labelframe_gestion, text="Sauvgarder JSON")
-        self.bouton_sauvgarder_json.grid(row=1, column=1, sticky="")
+        self.bouton_sauvgarder_json = tk.Button(self.labelframe_gestion, text="Sauvgarder JSON", command=self.exporterJson)
+        self.bouton_sauvgarder_json.grid(row=1, column=1, sticky="", pady=(10,20))
 
         self.bouton_importer_csv = tk.Button(self.labelframe_gestion, text="Importer CSV")
-        self.bouton_importer_csv.grid(row=1, column=2, sticky="")
+        self.bouton_importer_csv.grid(row=1, column=2, sticky="", pady=(10,20))
 
-        self.bouton_importer_json = tk.Button(self.labelframe_gestion, text="Importer JSON")
-        self.bouton_importer_json.grid(row=1, column=3, sticky="")
+        self.bouton_importer_json = tk.Button(self.labelframe_gestion, text="Importer JSON", command= self.importerJson)
+        self.bouton_importer_json.grid(row=1, column=3, sticky="", pady=(10,20))
 
 
         self.frame_tableau = tk.Frame(content)
-        self.frame_tableau.grid(row=2, column=0, sticky="nsew")
+        self.frame_tableau.grid(row=2, column=0, sticky="nsew", padx= 10, pady= 10)
+
+        self.frame_tableau.columnconfigure(0, weight=1)
+        self.frame_tableau.rowconfigure(1, weight=1)
 
         self.tree = ttk.Treeview(self.frame_tableau, columns=("produit", "quantite", "prix"), show="headings")
         self.tree.heading("produit", text="Produit")
@@ -122,6 +136,79 @@ class App(tk.Tk):
 
         self.tree.grid(column=0, row=0, sticky="nsew")
 
+
+    def ajouter(self):
+        produit = self.entry_produit.get()
+        quantite = self.entry_quantite.get()
+        prix = self.entry_prix.get()
+        if not produit:
+            messagebox.showwarning("Validation", "Le nom est requis.")
+            return
+        if quantite and not quantite.isdigit():
+            messagebox.showerror("Validation", "Âge doit être un entier.")
+            return
+        if prix and not prix.isdigit():
+            messagebox.showerror("Validation", "Âge doit être un entier.")
+            return
+
+        self.tree.insert("", "end", values=(produit, quantite, prix))
+        self.rows.append({
+            "Produit": produit,
+            "Quantité": quantite,
+            "Prix": prix
+        })
+        self.entry_produit.delete(0, "end")
+        self.entry_quantite.delete(0, "end")
+        self.entry_prix.delete(0, "end")
+
+
+
+    def supprimer(self):
+        selected_item = self.tree.selection()  # Récupère l'élement sélectionné
+        if selected_item:
+            self.tree.delete(selected_item)  # Supprime l'élement
+        else:
+            messagebox.showwarning("Avertissement", "Aucun élement selectionné.")
+
+
+
+
+
+    def exporterJson(self):
+        if not self.rows:
+            messagebox.showinfo("export json", "Rien à  exporter")
+            return
+        path = filedialog.asksaveasfilename(title="Exporter Json",defaultextension=".json", filetypes=[("Json", "*.json"), ("All files", "*.*")])
+        if not path:
+            return
+        try:
+            with open(path, "w", encoding="utf-8") as f:
+                json.dump(self.rows, f, indent=4)
+            messagebox.showinfo("export json", "Exporter JSON exported.")
+        except exception as e:
+            messagebox.showerror("echec export json", str(e))
+
+
+    def importerJson(self):
+        path = filedialog.askopenfilename(title="Importer Json", filetypes=[("Json", "*.json"), ("All files", "*.*")])
+        if not path:
+            return
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+            if not isinstance(data, list):
+                raise ValueError("le json doit contenir unr liste d'objet")
+            self.rows = []
+            for r in data:
+                if isinstance(r, dict):
+                    ligne ={}
+                    for k in FIELDS:
+                        ligne[k] = r.get(k,"")
+                    self.rows.append(ligne)
+            # self.refresh()
+            messagebox.showinfo("importer json", "Importer JSON imported.")
+        except exception as e:
+            messagebox.showerror("echec importer json", str(e))
 
 
 
